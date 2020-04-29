80ffdbf9947b602e748b9780298e0c3b70d6417e
Arpit Kohli <arpit.kohli@nagarro.com>
2020-04-29 11:20:05 +0530

    Initial Commit

---
+++ MathOperation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MathOperation
+//
+//  Created by Arpit Kohli on 29/04/20.
+//  Copyright © 2020 Arpit Kohli. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
